24e4b3ab78ca84d370f4a5e6ac96bc821839af1a
Jorge Eduardo Garcia Castro <23jegc1990@gmail.com>
2017-06-05 20:13:14 -0500

    SumaDEMultiplos
    Suma de multiplos basicos, en c#

---
+++ SumaDeMultiplosV1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDeMultiplosV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sumarmultiplos smulti = new sumarmultiplos();            
+            Console.Write("La suma de los multiplos: "+ smulti.sumMultiplos());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SumaDeMultiplosV1/sumarmultiplos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDeMultiplosV1
+{
+    class sumarmultiplos
+    {
+        public int sumMultiplos()
+        {
+            int suma = 0;
+            for (int i=1;i<1000;i++)
+            {
+                if (i%3==0 || i%5==0)
+                {
+                    suma =suma + i;
+                }
+            }
+            return suma;
+        }
+    }
+
+    
+}
